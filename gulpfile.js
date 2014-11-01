fe0547689b6561755801030d0c94f05315e29e7b
JC Ivancevich <jc.ivancevich@gmail.com>
2014-10-31 22:45:22 -0300

    gulp improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 /* jshint node: true, browser: false */
 'use strict';
+var spawn = require('child_process').spawn;
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var connect = require('gulp-connect');
-var plumber = require('gulp-plumber');
-var shell = require('gulp-shell');
 
 gulp.task('server', function() {
     connect.server({
@@ -13,9 +13,19 @@ gulp.task('server', function() {
 });
 
 gulp.task('integration-tests', function() {
-    gulp.src('')
-        .pipe(plumber())
-        .pipe(shell(['casperjs test ./test/index.js']));
+    var tests = ['test/index.js'];
+    var casperChild = spawn('casperjs', ['test'].concat(tests));
+    casperChild.stdout.on('data', function(data) {
+        gutil.log('CasperJS:', data.toString().slice(0, -1));
+    });
+    casperChild.on('close', function(code) {
+        if (code === 0) {
+            console.error('Done with errors');
+        } else {
+            console.log('Done with success');
+        }
+        process.exit(code);
+    });
 });
 
 gulp.task('test', ['server', 'integration-tests']);
