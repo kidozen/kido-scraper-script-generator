5b78cfb9f91593d669243c5bc5d3f224127cae2d
JC Ivancevich <jc.ivancevich@gmail.com>
2014-11-07 13:53:04 -0300

    gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,23 @@
 /* jshint node: true, browser: false */
 'use strict';
+var glob = require('glob');
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+var srcFiles = glob.sync('./extension/script/devtools/**/*.js');
+
+gulp.task('browserify', function() {
+    return browserify('./extension/script/devtools/panel.js')
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./extension/'));
+});
+
+gulp.task('browserify-watch', function() {
+    gulp.watch(srcFiles, ['browserify']);
+});
 
 gulp.task('server', function() {
     connect.server({
@@ -9,3 +25,5 @@ gulp.task('server', function() {
         port: 9090
     });
 });
+
+gulp.task('dev', ['browserify-watch', 'server']);
