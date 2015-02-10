kidozen/kido-scraper-script-generator

commit 8c342f7d3c9bff80a6da5b2d935169abac5faa1d

Added the ability of editing and deleting steps within a certain project. One small bug has advertently been introduced: after the second time the user tries to edit the same step of type "scrape", the "All fields belong to the same container" checkbox will (wrongly) be unchecked even though the "container" property is not blank. This bug has been logged properly in Asana and will be tackled and fixed soon.

extension/script/devtools/controllers/project/step/StepEditController.js
@@ -1,5 +1,6 @@
 'use strict';
 require('angular');
+var _ = require('lodash');
 var Site = require('../../../model/Site');
 
 module.exports = (function () {
@@ -7,22 +8,34 @@ module.exports = (function () {
     angular.module('KidoScraper').controller('StepEditController', function ($scope, $routeParams, $location, RunInBackgroundScript, AngularScope) {
         console.log('Loading Step Edit Controller...');
 
-        if (!$routeParams.name || !$routeParams.type) {
+        if (!$routeParams.siteName || !$routeParams.type) {
             return $location.path('/');
         }
-        RunInBackgroundScript.getFromLocalStorage($routeParams.name).done(function (siteAsJson) {
+
+        $scope.creationMode = !$routeParams.stepName;
+        $scope.editionMode = $routeParams.stepName;
+
+        RunInBackgroundScript.getFromLocalStorage($routeParams.siteName).done(function (siteAsJson) {
             AngularScope.apply($scope, function () {
                 $scope.site = siteAsJson;
+
+                if ($scope.editionMode) {
+                    var existingStep = _.find($scope.site.steps, function(s){ return s.name === $routeParams.stepName; });
+                    if (!existingStep) return $location.path('/projects/' + $scope.site.name);
+
+                    $scope.currentStep = existingStep;
+                } else {
+                    $scope.currentStep = Site.getDefaults($routeParams.type);
+                }
             });
         });
-        $scope.currentStep = Site.getDefaults($routeParams.type);
-        $scope.isForm = $scope.currentStep.type === Site.TYPES.FORM;
-        $scope.isClick = $scope.currentStep.type === Site.TYPES.CLICK;
-        $scope.isScrape = $scope.currentStep.type === Site.TYPES.SCRAPE;
+        $scope.isForm = $routeParams.type === Site.TYPES.FORM;
+        $scope.isClick = $routeParams.type === Site.TYPES.CLICK;
+        $scope.isScrape = $routeParams.type === Site.TYPES.SCRAPE;
 
         $scope.submit = function () {
             try {
-                Site.validateStep($scope.currentStep);
+                Site.validateStep($scope.currentStep, $scope.site, $scope.creationMode);
             } catch (exception) {
                 return alert(exception.toString());
             }
@@ -30,15 +43,22 @@ module.exports = (function () {
                 alert("$scope.site is required!");
                 return;
             }
-            $scope.site.steps.push($scope.currentStep);
 
-            RunInBackgroundScript.setInLocalStorage({
-                key: $routeParams.name, value: new Site($scope.site).toJson()
-            }).done(function () {
-                AngularScope.apply($scope, function () {
-                    $location.path('/projects/' + $scope.site.name);
+            if ($scope.creationMode) {
+                $scope.site.steps.push($scope.currentStep);
+                saveSiteInLocalStorage();
+            } else {
+                RunInBackgroundScript.getFromLocalStorage($routeParams.siteName).done(function (site) {
+                    var step2EditIdx = _.findIndex(site.steps, function(s){ return s.name === $routeParams.stepName; });
+                    if (step2EditIdx === -1) {
+                        alert("There was an error when attempting to update the step " + $routeParams.stepName);
+                        return;
+                    }
+                    $scope.site.steps.splice(step2EditIdx, 1, $scope.currentStep);
+
+                    saveSiteInLocalStorage();
                 });
-            });
+            }
         };
         $scope.cancel = function () {
             if (!$scope.site) {
@@ -47,5 +67,14 @@ module.exports = (function () {
             }
             $location.path('/projects/' + $scope.site.name);
         };
+
+        function saveSiteInLocalStorage() {
+            RunInBackgroundScript.setInLocalStorage({key: $routeParams.siteName, value: new Site($scope.site).toJson()})
+                .done(function () {
+                    AngularScope.apply($scope, function () {
+                        $location.path('/projects/' + $scope.site.name);
+                    });
+                });
+        }
     });
 })();

extension/script/devtools/controllers/project/ProjectDetailsController.js
@@ -18,7 +18,7 @@ module.exports = (function () {
             name: 'Click'
         }, {
             id: Site.TYPES.SCRAPE,
-            name: 'Scrap'
+            name: 'Scrape'
         }];
         $scope.stepType = $scope.types[0].id;
         RunInBackgroundScript.getFromLocalStorage($routeParams.name).done(function(siteAsJson) {
@@ -48,6 +48,22 @@ module.exports = (function () {
                 };
                 $scope.runSite = function (site) {
                     $location.path('/projects/run/' + site.name);
+                };
+                $scope.editStep = function (step) {
+                    $location.path('/projects/step/edit/' + $scope.site.name + '/' + step.type + '/' + step.name);
+                };
+                $scope.deleteStep = function (index) {
+                    var stepName = $scope.site.steps[index].name;
+
+                    if (!confirm("Are you sure you want to delete the step '" + stepName + "'?")) {
+                        return;
+                    }
+                    $scope.site.steps.splice(index, 1);
+
+                    RunInBackgroundScript.setInLocalStorage({
+                        key: $scope.site.name,
+                        value: new Site($scope.site).toJson()
+                    });
                 };
             });
         });

extension/script/devtools/directives/StepFormDirective.js
@@ -15,7 +15,7 @@ module.exports = (function () {
             link: function (scope) {
                 if (!scope.currentStep) throw 'stepForm directive needs a currentStep to work with';
 
-                scope.validParamTypes = ['string', 'number', 'boolean', 'array', 'object', 'userEmail', 'claim', 'timestamp'];
+                //scope.validParamTypes = ['string', 'number', 'boolean', 'array', 'object', 'userEmail', 'claim', 'timestamp'];
 
                 scope.addSelector = function () {
                     scope.currentStep.selectors.push(Site.getDefaults(Site.TYPES.FORM_SELECTOR));

extension/script/devtools/directives/StepScrapeDirective.js
@@ -19,7 +19,9 @@ module.exports = (function () {
                     id: ATTRS.TEXT,
                     name: 'Text'
                 }];
-                scope.scrapeWithinContainer = false;
+                // FIXME -- Known-bug: After the second time a user reaches the page rendered by this directive,
+                // FIXME -- the checkbox is not checked because scope.scrapeWithinContainer is undefined
+                scope.scrapeWithinContainer = (scope.currentStep.container && scope.currentStep.container.length > 0);
 
                 // blank container property every time the checkbox gets unchecked
                 scope.$watch('scrapeWithinContainer', function (enabled) {

extension/script/devtools/model/Site.js
@@ -7,6 +7,7 @@ var StepFormSelector = require('./StepFormSelector');
 var StepScrape = require('./StepScrape');
 var StepSelector = require('./StepSelector');
 var beautify = require('js-beautify').js_beautify;
+var _ = require('lodash');
 
 module.exports = (function() {
 
@@ -71,9 +72,17 @@ module.exports = (function() {
         };
     };
 
-    Site.validateStep = function(step) {
+    Site.validateStep = function(step, site, forCreation) {
         if (!step) throw 'The "step" argument is required';
+        if (!step.name) throw 'The "step.name" property is required';
         if (!step.type) throw 'The "step.type" property is required';
+
+        if (forCreation) {
+            var existingStepWithSameName = _.find(site.steps, function (s) {
+                return s.name === step.name;
+            });
+            if (existingStepWithSameName) throw 'Duplicated step name: ' + step.name;
+        }
         var Constructor = Site.getConstructor(step.type);
         try {
             step = new Constructor(Site, step);

extension/script/devtools/model/StepFormSelector.js
@@ -68,6 +68,10 @@ module.exports = (function() {
             if (param.name.toString().match(/^[-a-zA-Z0-9,&]+$/) == null) {
                 throw 'Param name must contain only alphanumeric characters';
             }
+            //FIXME Once we start actually supporting more kind of params, this IF condition should go away
+            if (!param.type) {
+                param.type = 'string';
+            }
         }
         return param;
     };

extension/script/devtools/panel.js
@@ -31,7 +31,7 @@ angular.module('KidoScraper', ['ngRoute', 'angular-loading-bar', 'ui.bootstrap']
                 templateUrl: 'partial/project/project_detail.html',
                 controller: 'ProjectDetailsController'
             })
-            .when('/projects/step/edit/:name/:type', {
+            .when('/projects/step/edit/:siteName/:type/:stepName?', {
                 templateUrl: 'partial/project/step/step_edit.html',
                 controller: 'StepEditController'
             })

extension/view/devtools/partial/project/project_detail.html
@@ -19,8 +19,12 @@
         </button>
         <table class="table" style="margin:10px">
             <tr ng-repeat="s in site.steps">
-                <td>
-                    {{ s.name }} ({{ s.type }})
+                <td class="two-cols-column-long">
+                    <label>{{ s.name }} ({{ s.type }})</label>
+                </td>
+                <td class="two-cols-column-short">
+                    <a class="btn btn-primary" ng-click="editStep(s)">Edit</a>
+                    <a class="btn btn-danger" ng-click="deleteStep($index)">Delete</a>
                 </td>
             </tr>
         </table>

commit b736eaa35ea686ef19d8545233a7f429c5a16783

Fixed the bug introduced by the commit 8c342f7d3c9bff80a6da5b2d935169abac5faa1d, where the button "Select it!" stopped working after cancelling a step creation/edition. It was caused, oddly, by the absence of a "currentStep" property in the scope of StepEditController.

extension/script/devtools/controllers/project/step/StepEditController.js
@@ -14,6 +14,8 @@ module.exports = (function () {
         $scope.creationMode = !$routeParams.stepName;
         $scope.editionMode = $routeParams.stepName;
 
+        $scope.currentStep = Site.getDefaults($routeParams.type);
+
         $scope.breadcrumbReplacements = {
             'Project Name': $routeParams.name,
             'Step Type or Name': $scope.editionMode ? $routeParams.stepName : "New " + $routeParams.type + " step"
@@ -28,8 +30,6 @@ module.exports = (function () {
                     if (!existingStep) return $location.path('/projects/' + $scope.site.name);
 
                     $scope.currentStep = existingStep;
-                } else {
-                    $scope.currentStep = Site.getDefaults($routeParams.type);
                 }
             });
         });